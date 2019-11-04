05f76a327bc48e19c431a568ae9f7e684135c6ab
MarkusHeine <markusheine.dev@gmail.com>
2019-11-04 14:48:32 +0100

    add all

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// import routes
+const authRoute = require("./routes/auth");
+
+// connect to db
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("connected to db")
+);
+
+//middelwares
+app.use(express.json());
+
+//route middelwares
+app.use("/api/user", authRoute);
+
+app.listen(3000, () => console.log("Listening in 3000 FM"));
